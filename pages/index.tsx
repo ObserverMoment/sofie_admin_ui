f59ab4713b82465d9dc192f3a7a171412b7ba1d5
ObserverMoment <bosonsoul@gmail.com>
2021-02-08 17:13:30 +0000

    basic skeleton

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,11 @@
+import React from 'react'
+import { Page } from '../components/layout/page'
 import { Title } from '../components/styled'
 
 export default function Home() {
-  return <Title>My page: Home</Title>
+  return (
+    <Page>
+      <Title>Home Page</Title>
+    </Page>
+  )
 }
